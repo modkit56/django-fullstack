f39beab43703f55fb66523beff7d9cbaba8adda7
modkit56 <siddharth.vagavolu@uwaterloo.ca>
2022-02-16 22:31:25 -0500

    Class based Views
    - explored class-based views and replaced current function-based views with them

--- sid_website/blog/urls.py
+++ sid_website/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.induvidual_post, name="induvidual-post-page") # is SEO friendly posts/my-first-post
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.InduvidualPostView.as_view(), name="induvidual-post-page") # is SEO friendly posts/my-first-post
 ]
--- sid_website/blog/views.py
+++ sid_website/blog/views.py
@@ -1,24 +1,54 @@
+from re import template
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from . models import Post
 
 # Create your views here.
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3] # this changes to a SQL Query (no performance hit)
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts" # provided under this name
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def induvidual_post(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug) # slug on the right side is the argument of the function, other one is attribute
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# no need to query manually as we are inheriting from the DefaultView which does the job for us
+class InduvidualPostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3] # this changes to a SQL Query (no performance hit)
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
+
+# def induvidual_post(request, slug):
+#     identified_post = get_object_or_404(Post, slug=slug) # slug on the right side is the argument of the function, other one is attribute
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "post_tags": identified_post.tags.all()
+#     })
