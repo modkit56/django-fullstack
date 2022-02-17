624717a9ec1108e29c08fd77f26da6eef27151ca
modkit56 <siddharth.vagavolu@uwaterloo.ca>
2022-02-16 22:10:29 -0500

    File Uploads
    - added the feature of uploading post images from admin panel
    - changed the templates accordingly

--- sid_website/sid_website/settings.py
+++ sid_website/sid_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from tkinter import BaseWidget
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/files/"
