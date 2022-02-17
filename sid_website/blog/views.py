86bbef8f7aac07bbed13d5c1128ba55ce1374c86
modkit56 <siddharth.vagavolu@uwaterloo.ca>
2022-02-17 14:39:53 -0500

    Comment Submission
    - making sure comment submit works (doesn't display the comments on the website yet!)
    - did some server side error handling as frontend validation is not that secure

--- sid_website/blog/views.py
+++ sid_website/blog/views.py
@@ -1,6 +1,8 @@
-from re import template
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from . models import Post
 from . forms import CommentForm
@@ -24,16 +26,33 @@ class AllPostsView(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-# no need to query manually as we are inheriting from the DefaultView which does the job for us
-class InduvidualPostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class InduvidualPostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+          "post": post,
+          "post_tags": post.tags.all(),
+          "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+          comment = comment_form.save(commit=False)
+          comment.post = post
+          comment.save()
+          
+          return HttpResponseRedirect(reverse("induvidual-post-page", args=[slug]))
+
+        context = {
+          "post": post,
+          "post_tags": post.tags.all(),
+          "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
 
 
 # def starting_page(request):
