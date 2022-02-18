6db4eacc23fc0121ccb3d9b252f7994345bddc38
modkit56 <siddharth.vagavolu@uwaterloo.ca>
2022-02-17 20:01:11 -0500

    Stored Posts
    - ability to store and un-store a post
    - some more styling
    - some manual testing to make sure it's not breaking on the surface level

--- sid_website/blog/urls.py
+++ sid_website/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts", views.AllPostsView.as_view(), name="posts-page"),
-    path("posts/<slug:slug>", views.InduvidualPostView.as_view(), name="induvidual-post-page") # is SEO friendly posts/my-first-post
+    path("posts/<slug:slug>", views.InduvidualPostView.as_view(), name="induvidual-post-page"), # is SEO friendly posts/my-first-post
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
